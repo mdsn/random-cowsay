1384fcb4e240b144abfdc1ded5c2aee43579e6f4
Mariano Casco <mariano.c88@gmail.com>
2016-07-11 21:17:35 -0300

    Narrow cowfiles, use short fortunes only

--- random-cowsay.sh
+++ random-cowsay.sh
@@ -1,11 +1,12 @@
 # random-cowsay.sh
 # pick a random cowsay(1) cowfile to output an adage from fortune(6)
 
+cowfiles="moofasa telebears flaming-sheep moose three-eyes mutilated
+          head-in hellokitty satanic jkh sheep skeleton udder small
+          default sodomized www"
+
+cowfile=$(echo "$cowfiles" | tr -s ' ' '\n' | random -f - | head -n 1)
+
 if [ -x $(which fortune) -a -x $(which cowsay) ]; then
-    fortune all | cowsay -f $(
-        cowsay -l   |
-        tail -n +2  |
-        tr ' ' '\n' |
-        random -f - |
-        head -n 1  );
+    fortune -s | cowsay -f "$cowfile"
 fi
